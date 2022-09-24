c31852d161e92ae1b520453fe935682e939259df
bhavyapatel1551 <bhavyapatel20@gnu.ac.in>
2022-09-24 18:16:05 +0530

    Practical

--- app/src/main/java/com/example/madpractical7_20012011079/MainActivity.kt
+++ app/src/main/java/com/example/madpractical7_20012011079/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.madpractical7_20012011079
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var alarmbutton=findViewById<Button>(R.id.alrmbtn1)
+        alarmbutton.setOnClickListener{
+            showTimeDilog()
+
+        }
     }
-}
+    private fun showTimeDilog(){
+        var calendar:Calendar=Calendar.getInstance()
+        var hour:Int=calendar.get(Calendar.HOUR_OF_DAY)
+        var minute:Int=calendar.get(Calendar.MINUTE)
+        }
+    }
