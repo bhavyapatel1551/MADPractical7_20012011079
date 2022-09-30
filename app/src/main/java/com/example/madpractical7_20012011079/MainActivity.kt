1937e156ff824c43256d6099e330fc872c24546f
Student <bhavyapatel20@gnu.ac.in>
2022-09-30 12:23:39 +0530

    Pr

--- app/src/main/java/com/example/madpractical7_20012011079/MainActivity.kt
+++ app/src/main/java/com/example/madpractical7_20012011079/MainActivity.kt
@@ -14,13 +14,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var alarmbutton=findViewById<Button>(R.id.alrmbtn1)
-        alarmbutton.setOnClickListener{
+        var alarmbutton = findViewById<Button>(R.id.alrmbtn1)
+        alarmbutton.setOnClickListener {
             showTimeDialog()
         }
     }
-    fun showTimeDialog()
-    {
+
+    fun showTimeDialog() {
         val cldr: Calendar = Calendar.getInstance()
         val hour: Int = cldr.get(Calendar.HOUR_OF_DAY)
         val minutes: Int = cldr.get(Calendar.MINUTE)
@@ -35,19 +35,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun sendDialogDataToActivity(sHour: Int, sMinute: Int) {
-        val alarmCalendar=Calendar.getInstance()
-        val year:Int=alarmCalendar.get(Calendar.YEAR)
-        val month:Int=alarmCalendar.get(Calendar.MONTH)
-        val day:Int=alarmCalendar.get(Calendar.DATE)
-        alarmCalendar.set(year,month,day,sHour,sMinute,0)
-        binding.textAlarmTime.text=SimpleFormatter("hh:mm:ss a").format(alarmCalendar.time)
+        val alarmCalendar = Calendar.getInstance()
+        val year: Int = alarmCalendar.get(Calendar.YEAR)
+        val month: Int = alarmCalendar.get(Calendar.MONTH)
+        val day: Int = alarmCalendar.get(Calendar.DATE)
+        alarmCalendar.set(year, month, day, sHour, sMinute, 0)
     }
-    override  fun setAlarm(millisTime:Long,str:String)
-    {
-        val intent = Intent(this,MyReceiver::class.java)
-        intent.putExtra("Service1",str)
-        val pendingIntent=PendingIntent.getBroadcast()
+
+    private fun setAlarm(millisTime: Long, str: String) {
+        val intent = Intent(this, MyReceiver::class.java)
+        intent.putExtra("Service1", str)
+        val pendingIntent = PendingIntent.getBroadcast()
     }
+}
 
 
 
